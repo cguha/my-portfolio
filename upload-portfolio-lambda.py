709403bfa72545966c25ec3fada3d43b718894e3
Chandradip Guha <dip@giffgaff.co.uk>
2018-01-09 22:18:49 +0000

    added

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,16 @@
+import boto3
+import StringIO
+import zipfile
+
+s3 = boto3.resource('s3')
+portfolio_bucket = s3.Bucket('kubernetes.cguha.tech')
+build_bucket = s3.Bucket('portfoliobuild.cguha.tech')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('buildportfolio.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for name in myzip.namelist():
+        obj = myzip.open(name)
+        portfolio_bucket.upload_fileobj(obj, name)
+        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
