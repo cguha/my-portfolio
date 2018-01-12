3bbf0c31dc311f9ff1f75ef21b62674c1b98495c
Chandradip Guha <dip@giffgaff.co.uk>
2018-01-12 12:31:42 +0000

    webpack

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("react-dom is loaded in main.js")
-console.log("webpack run chec")
+const myWork = [
+  {
+    'title': "Microservices Architecture",
+    'image': {
+      'desc':"Example 1",
+      'src':"images/example1.png",
+      'comment':""
+    }
+  },
+  {
+    'title': "AWS Cloud",
+    'image': {
+      'desc':"Example 2",
+      'src':"images/example2.png",
+      'comment':""
+    }
+  },
+  {
+    'title': "CI and CD deliver",
+    'image': {
+      'desc':"Example 3",
+      'src':"images/example3.png",
+      'comment':""
+    }
+  }
+]
+
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
